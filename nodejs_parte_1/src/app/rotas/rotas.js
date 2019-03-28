fa51c9ad6728375cbdd16b567881dc20fae4d952
BrunoASN <BrunoASN@users.noreply.github.com>
2019-03-28 09:41:04 -0300

    Promises

--- nodejs_parte_1/src/app/rotas/rotas.js
+++ nodejs_parte_1/src/app/rotas/rotas.js
@@ -1,9 +1,11 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
 //Mostra o caminho que a aplicação tem que tomar; get
 module.exports = (app) => {
-    app.get(`/`,function(req,resp) {
-        //Função que diz o que vai ser enviado para o leitor
-        resp.send(
-          `
+  app.get(`/`, function (req, resp) {
+    //Função que diz o que vai ser enviado para o leitor
+    resp.send(
+      `
             <html>
               <head>
                 <meta charset="utf-8">
@@ -13,12 +15,21 @@ module.exports = (app) => {
               </body>
             </html>
           `
-        );
-    });
-      
-    app.get(`/livros`,function(req,resp) {
-        resp.marko(
-          require('../views/livros/lista/lista.marko')
-        );
-    });
+    );
+  });
+
+
+  app.get('/livros', function (req, resp) {
+
+    const livroDao = new LivroDao(db);
+    livroDao.lista()
+      .then(livros => resp.marko(
+        require('../views/livros/lista/lista.marko'), {
+          livros: livros
+        }
+      ))
+      .catch(erro => console.log(erro));
+
+
+  });
 };
